62b71c43cabbf3fa697f03a82b805e49d34cee52
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2022-02-10 18:50:56 -0500

    Update pull_cutout_loop.py

--- pull_cutout_loop.py
+++ pull_cutout_loop.py
@@ -7,11 +7,11 @@ import glob
 num_files = 100
 
 # path = '/arc/home/jkavelaras/'
-path = 'vos:OSSOS/measure3/2015A-P/' # 15AP+2-1_p36.measure3.cands.astrom'
+path = 'vos:OSSOS/measure3/2015A-P/15AP+2-1/' # 15AP+2-1_p36.measure3.cands.astrom'
 # vos:OSSOS/measure3/${BLOCK} - use this to run through diff blocks
 
 
-contents = os.popen('vls vos:OSSOS/measure3/2015A-P').read().split('\n')
+contents = os.popen('vls vos:OSSOS/measure3/2015A-P/15AP+2-1/').read().split('\n')
 print(len(contents))
 #real_files = glob.glob(path + "/*.reals.astrom")
 #print('Number of .reals:', len(real_files))
