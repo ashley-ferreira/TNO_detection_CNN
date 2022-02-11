1f887e732877c40780b625283dc1cba03207ac21
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2022-02-10 20:30:29 -0500

    Update pull_cutout_func.py

--- pull_cutout_func.py
+++ pull_cutout_func.py
@@ -26,6 +26,7 @@ def pull_cutout(full_filename='vos:OSSOS/measure3/2015A-P/15AP+2-1/15AP+2-1_p36.
         for i,reading in enumerate(source.get_readings()):
             reading.uncertainty_ellipse.a = cutout_size/0.185/2/2.5 * units.arcsecond
             print(reading)
+            print(reading.uncertainty_ellipse)
             cutout = dlm.download_cutout(reading, needs_apcor=True)
             print(cutout.hdulist)
             cutout.hdulist.writeto('/arc/projects/uvickbos/ML-MOD/new_pixsize/'+ filename + str(i) + '_label=' + str(real_exists) + '.fits', overwrite=True)
