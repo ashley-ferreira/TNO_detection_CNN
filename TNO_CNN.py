a483387851f3d67683cffbe7e6b9ef2b2a6c863f
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2022-02-16 13:21:45 -0500

    Update TNO_CNN.py

--- TNO_CNN.py
+++ TNO_CNN.py
@@ -257,7 +257,7 @@ def convnet_model(input_shape, training_labels, dropout_rate=dropout_rate):
     #hidden layer 1
     model.add(Conv3D(filters=16, kernel_size=(3, 3, 1), input_shape=input_shape, activation='relu', padding='same'))
     model.add(Dropout(dropout_rate))
-    model.add(MaxPool3D(pool_size=(2, 2, 2), padding='valid'))
+    model.add(MaxPool3D(pool_size=(1, 1, 1), padding='valid'))
 
     #hidden layer 2 with Pooling
     model.add(Conv3D(filters=16, kernel_size=(3, 3, 1), input_shape=input_shape, activation='relu', padding='same'))
