d29e91c03727ed0830dc4eb9bb4494b9e6203418
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2022-02-23 12:21:04 -0500

    Update TNO_CNN.py

--- TNO_CNN.py
+++ TNO_CNN.py
@@ -262,19 +262,19 @@ def convnet_model(input_shape, training_labels, unique_labs, dropout_rate=dropou
     #hidden layer 1
     model.add(Conv3D(filters=16, kernel_size=(3, 3, 1), input_shape=input_shape, activation='relu', padding='same'))
     model.add(Dropout(dropout_rate))
-    model.add(MaxPool3D(pool_size=(1, 1, 1), padding='valid')) # padding='valid'
+    model.add(MaxPool3D(pool_size=(2, 2, 2), padding='valid')) # padding='valid'
 
     #hidden layer 2 with Pooling
     model.add(Conv3D(filters=16, kernel_size=(3, 3, 1), input_shape=input_shape, activation='relu', padding='same'))
     model.add(Dropout(dropout_rate))
-    model.add(MaxPool3D(pool_size=(1, 1, 1), padding='valid'))
+    model.add(MaxPool3D(pool_size=(2, 2, 2), padding='valid'))
 
     model.add(BatchNormalization())
 
     #hidden layer 3 with Pooling
     model.add(Conv3D(filters=8, kernel_size=(3, 3, 1), input_shape=input_shape, activation='relu', padding='same'))
     model.add(Dropout(dropout_rate))
-    model.add(MaxPool3D(pool_size=(1, 1, 1), padding='valid'))
+    model.add(MaxPool3D(pool_size=(2, 2, 2), padding='valid'))
 
     model.add(Flatten())
     model.add(Dense(32, activation='sigmoid'))
