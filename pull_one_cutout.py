c6c257fc8e1ec19b2acdf5ce86a8d34364dcd96f
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2022-02-28 18:32:47 -0500

    Update pull_one_cutout.py

--- pull_one_cutout.py
+++ pull_one_cutout.py
@@ -38,7 +38,7 @@ def pull_cutout(full_filename='vos:OSSOS/measure3/2015A-P/15AP+0+0/15AP+0+0_p34.
         for i,reading in enumerate(source.get_readings()):
             print(reading)
             cutout = dlm.download_cutout(reading, needs_apcor=True)
-            cutout.hdulist.writeto(sub_dir + filename + str(cand) + str(i) + '.fits', overwrite=True)
+            cutout.hdulist.writeto(sub_dir + filename + '_' + str(cand) + '_' + str(i) + '.fits', overwrite=True)
             mjd_obs = float(cutout.fits_header.get('MJD-OBS'))
             exptime = float(cutout.fits_header.get('EXPTIME'))
 
