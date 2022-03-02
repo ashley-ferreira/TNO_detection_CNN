2a829223ce3bab322e08fc9f992fbec95c1b18ae
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2022-03-02 12:38:47 -0500

    reorg for new cutout method

---
+++ pull_cutout.py
@@ -0,0 +1,75 @@
+from ossos import storage
+from ossos import astrom
+from ossos.downloads.cutouts import downloader
+
+from astropy import units
+
+import sys 
+import os
+
+from ossos.gui import logger
+
+storing_directory = '/arc/projects/uvickbos/ML-MOD/new_cutouts/'
+
+
+def pull_cutout(full_filename='vos:OSSOS/measure3/2015A-P/15AP+0+0/15AP+0+0_p14.measure3.cands.astrom', 
+        filename='15AP+0+0_p14.measure3.cands.astrom', real_exists=0):
+    '''
+    
+    '''
+    storage.DBIMAGES = 'vos:OSSOS/dbimages'
+    storage.MEASURE3 = 'vos:OSSOS/measure3'
+
+    parser = astrom.AstromParser()
+    dlm = downloader.ImageCutoutDownloader()
+    sources = parser.parse(full_filename)
+
+    if real_exists:
+        # make a list of loc to look at 
+        # HERE
+        real_full_filename = full_filename.replace('.cands.astrom', '.reals.astrom') 
+        real_sources = parser.parse(real_full_filename)
+
+        real_cands = []
+        for real_source in real_sources.get_sources(): 
+            print(real_source)
+
+            for i,real_reading in enumerate(real_source.get_readings()):
+                print(real_reading.dec)
+                real_cands.append(real_reading.dec)
+
+
+        cand=0
+        for source in sources.get_sources(): 
+            print(source)
+
+            file_dir = filename + '_' + str(cand) + '/'
+            sub_dir = storing_directory + file_dir
+            os.mkdir(sub_dir)
+            cand+=1
+            for i,reading in enumerate(source.get_readings()):
+                print(reading)
+
+                if reading.dec in real_cands:
+                    label=1
+                else:
+                    label=0
+
+                cutout = dlm.download_cutout(reading, needs_apcor=True)
+                cutout.hdulist.writeto(sub_dir + filename + '_' + str(i) + '_label=' + str(label) + '.fits', overwrite=True)
+
+    else:
+        label=0
+        cand=0
+        for source in sources.get_sources(): 
+            print(source)
+            file_dir = filename + '_' + str(cand) + '/'
+            sub_dir = storing_directory + file_dir
+            os.mkdir(sub_dir)
+            cand+=1
+            for i,reading in enumerate(source.get_readings()):
+                print(reading)
+                cutout = dlm.download_cutout(reading, needs_apcor=True)
+                cutout.hdulist.writeto(sub_dir + filename + '_' + str(i) + '_label=' + str(label) + '.fits', overwrite=True)
+
+pull_cutout(real_exists=1)
---
+++ pull_cutout_loop.py
@@ -0,0 +1,72 @@
+# RUN IN NW TERMINAL
+
+import sys 
+import os
+import glob
+from pull_cutout_func import pull_cutout
+
+
+
+def pull_dir_loop(cutout_dir = '2015A-P/', num_cutouts = 10000000):
+    year = cutout_dir[2:4]
+    print(year)
+    sem = cutout_dir[4:5]
+    print(sem)
+    block = cutout_dir[6:7]
+    print(block)
+
+    
+    og_name = cutout_dir.replace('_automatic/', '/')
+    vos_path = 'vos:OSSOS/measure3/' + og_name + '/'
+    local_path = '/arc/projects/uvickbos/ML-MOD/OSSOS_datapull/' + cutout_dir
+   
+    main_dirs = [directory for directory in os.listdir(local_path) if os.path.isdir(local_path+directory)]
+    print(main_dirs)
+
+    for d in main_dirs:
+        try: 
+            vos_path_d = vos_path + d
+            local_path_d = local_path +  d
+
+            str1 = 'ls ' + local_path_d
+            dirs = os.popen(str1).read().split('\n')
+
+            count = 0
+            for dir in dirs:
+                file = dir
+
+                file_cut = file.rsplit('/', 1)[-1]
+                print('checking file',file_cut)
+                
+                if count > num_cutouts//2:
+                    print('MAX CUTOUTS SAVED')
+                    break
+
+                elif file.endswith(".cands.astrom") and file_cut[9] == 'p': 
+                    file_path = os.path.join(str(vos_path_d)  + '/' + str(dir)) 
+                    real_file = file.replace('.cands.astrom', '.reals.astrom') 
+                    real_file_cut = real_file.rsplit('/', 1)[-1]
+                    #print('searching for .reals.astrom, found')
+                    print(real_file)
+                    real_exists = 0
+                        
+                    filesize = os.path.getsize(local_path_d + '/' + real_file_cut)
+                    print(filesize)
+
+                    if filesize != 0:
+                        count +=1
+                        print('real_exists = 1')
+                        real_exists = 1
+                       
+                    pull_cutout(str(file_path), str(file), real_exists)
+
+
+        except Exception as e:
+            print('ERROR with dir', d)
+            print(e)
+
+list_x = ['2015B-D_automatic/']
+# list_x = ['2013A-E_automatic/', '2013B-L_redo_automatic/', '2014B-H_fix_automatic/', '2015B-H_automatic/', '2015B-S_automatic/', '2015B-D_automatic/']
+# list_x = ['2015A-M_automatic/', '2013A-O_automatic/', '2015A-P_automatic/'] #'2015B-D_automatic/'
+for x in list_x:
+    pull_dir_loop(x)
